f1814360f4908f55424b3e3ed10279b09959fe24
david <davidealvarez1@gmail.com>
2020-03-12 22:16:34 -0700

    ContactForm.test.js

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 test("renders correctly", () => {
     render(<ContactForm />);
   });
-  
+
+  test("name entered", ()=>{
+      const{getByPlaceholderText} = render(<ContactForm/>);
+      const nameInput = getByPlaceholderText("bill");
+      fireEvent.change(nameInput);
+  })
