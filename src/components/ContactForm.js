291a82f143aa936a53b8b366e3b9a9ce6b725c69
david <davidealvarez1@gmail.com>
2020-03-15 20:44:37 -0700

    last commit as of 3/15

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,7 +18,7 @@ const ContactForm = () => {
           <input
             name="firstName"
             placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -55,7 +55,7 @@ const ContactForm = () => {
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submitBtn" type="submit" />
       </form>
     </div>
   );
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,12 +2,70 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
-test("renders correctly", () => {
-    render(<ContactForm />);
+test("renders ContactForm without crashing", () => {
+  render(<ContactForm />);
+});
+
+test('input[name="firstName"] should have placeholder "bill"', () => {
+  const { getByPlaceholderText } = render(<ContactForm />);
+  const input = getByPlaceholderText(/bill$/i);
+  expect(input).toBeInTheDocument();
+});
+
+test('ContactForm sets new contact to data', async () => {
+  const { getByLabelText, findByText, getByTestId } = render(<ContactForm />);
+
+  const firstNameInput = getByLabelText(/first name*/i);
+  const lastNameInput = getByLabelText(/last name*/i);
+  const emailInput = getByLabelText(/email*/i);
+  const messageInput = getByLabelText(/message/i);
+
+  fireEvent.change(firstNameInput, {
+    target: {
+      name: 'firstName',
+      value: 'John'
+    }
   });
 
-  test("name entered", ()=>{
-      const{getByPlaceholderText} = render(<ContactForm/>);
-      const nameInput = getByPlaceholderText("bill");
-      fireEvent.change(nameInput);
-  })
+  fireEvent.change(lastNameInput, {
+    target: {
+      name: 'lastName',
+      value: 'Doe'
+    }
+  });
+
+  fireEvent.change(emailInput, {
+    target: {
+      name: 'email',
+      value: 'myemail@gmail.com'
+    }
+  });
+
+  fireEvent.change(messageInput, {
+    target: {
+      name: 'message',
+      value: 'This is a message'
+    }
+  });
+
+  const submitBtn = getByTestId(/submitBtn/i);
+  fireEvent.click(submitBtn);
+
+  await findByText(/john/i);
+  await findByText(/doe/i);
+  await findByText(/myemail@gmail.com/i);
+  await findByText(/this is a message/i);
+
+});
+
+test('ContactForm should display errors if required fields aren\'t filled out', async () => {
+  const { findAllByText, getByTestId } = render(<ContactForm />);
+  const submitBtn = getByTestId(/submitBtn/i);
+  fireEvent.click(submitBtn);
+
+  const errors = await findAllByText(/Looks like there was an error: required/i);
+  expect(errors).toHaveLength(3);
+});
+
+
+
